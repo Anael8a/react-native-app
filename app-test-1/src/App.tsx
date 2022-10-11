a1a6c0be56a3e68f9118a9bbba3e5827a216a884
carlos8a <caanochoame@ittepic.edu.mx>
2022-10-11 10:38:39 -0500

    Segundo commit de react

--- app-test-1/src/App.tsx
+++ app-test-1/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Formularios } from "./components/formularios";
 //import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
 //import { TiposBasicos } from "./typescript/TiposBasicos";
 //import { Contador } from "./components/Contador";
 //import { ContadorHook } from "./components/ContadorHook";
 //import { Login } from "./components/Login";
-import { Usuarios } from "./components/Usuarios";
+//import { Usuarios } from "./components/Usuarios";
 const App = () => {
     return(
     <div>
@@ -14,7 +15,8 @@ const App = () => {
       {/*<Contador></Contador>*/}
       {/*<ContadorHook></ContadorHook>*/}
       {/*<Login></Login>*/}
-      {<Usuarios></Usuarios>}
+      {/*<Usuarios></Usuarios>*/}
+      {<Formularios></Formularios>}
     </div>
   )
 }
